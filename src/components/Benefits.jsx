2623f8f3accf4d7bc74ef02e84f0cefb613a99f5
Samuel Muli <samuelmuli95@gmail.com>
2024-07-23 22:35:27 +0300

    Create benefits section

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom"
+import Line from "../assets/Line 3.png"
+
+export default function Benefits() {
+    return (
+        <div className="py-10">
+            <div className="mb-10">
+							<h1 className="text-[4rem] font-light text-[#ffffff99]">Benefits</h1>
+						</div>
+            <div className="rounded-3xl border px-14 py-14 flex justify-between gap-16">
+                <div className="flex flex-col justify-between gap-4">
+                    <h4 className="font-medium text-2xl text-[#b9ff66]">Convenience</h4>
+                    <div>
+                        <p className="text-white text-lg">Avoid the hassle of running errands. With ErrandsPap, you can order anything you need from the comfort of your home and have it delivered straight to your door.</p>
+                    </div>
+                </div>
+
+                <img src={Line} alt="" />
+
+                <div className="flex flex-col justify-between gap-4">
+                    <h4 className="font-medium text-2xl text-[#b9ff66]">Time-saving</h4>
+                    <div>
+                        <p className="text-white text-lg">Free up your schedule for more important tasks or leisure activities. Our efficient delivery service ensures you spend less time shopping and more time enjoying life.</p>
+                    </div>
+                </div>
+
+                <img src={Line} alt="" />
+
+                <div className="flex flex-col justify-between gap-4">
+                    <h4 className="font-medium text-2xl text-[#b9ff66]">Real-Time Tracking</h4>
+                    <div>
+                        <p className="text-white text-lg">Stay informed about your order status with our real-time tracking feature. Know exactly when your items will arrive without any guesswork.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../common/Header"
 import Hero from "../components/Hero"
 import HowItWorks from "../components/HowItWorks"
+import Benefits from "../components/Benefits"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <HowItWorks />
+			<Benefits />
     </div>
   )
 }
