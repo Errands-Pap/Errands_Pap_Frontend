540840aab4dc6112572f6e2779e86f67a1ed8e0a
Samuel Muli <samuelmuli95@gmail.com>
2024-08-18 16:13:45 +0300

    Create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import ButtonPrimary from "../common/ButtonPrimary";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+	const [isVisible, setIsVisible] = useState(false)
+
+	const toggleVisibility = () => {
+		setIsVisible(!isVisible)
+	}
+
   return (
 		<div className="bg-[#06050f] flex w-full">
 			<img className="h-screen w-1/2 object-cover" src="../src/assets/Rectangle 79.png" alt="" />
@@ -10,19 +18,22 @@ export default function Login() {
 				<h1 className="text-[#b9ff66] text-5xl font-medium self-start mb-10">Login</h1>
 
 				<div className="flex flex-col w-full">
-					<form action="" className="flex flex-col">
+					<form action="" className="flex flex-col mb-8">
 						<label htmlFor="" className="text-white font-medium self-start mb-2">Email Address</label>
 						<input type="email" placeholder="Email Address" className="w-full bg-[#040409] focus:outline-none border border-[#c8cad0] p-3 rounded-xl text-white mb-10" />
 
 						<div className="flex flex-col relative">
 							<label htmlFor="" className="text-white font-medium self-start mb-2">Password</label>
-							<input type="password" placeholder="Password" className="w-full bg-[#040409] focus:outline-none border border-[#c8cad0] p-3 rounded-xl text-white mb-10" />
+							<input type={isVisible ? "text" : "password"} placeholder="Password" className="w-full bg-[#040409] focus:outline-none border border-[#c8cad0] p-3 rounded-xl text-white mb-10" />
+							<span class="material-symbols-outlined absolute right-5 top-11 cursor-pointer text-white" onClick={toggleVisibility}>
+								{isVisible ? "visibility" : "visibility_off"}
+							</span>
 						</div>
 
 						<ButtonPrimary>Login</ButtonPrimary>
 					</form>
 
-					<p>New User? <a href="">Sign Up Here</a></p>
+					<p className="text-white text-center text-sm font-normal">New User? <Link className="font-bold underline">Sign Up Here</Link></p>
 				</div>
 			</div>
 		</div>
