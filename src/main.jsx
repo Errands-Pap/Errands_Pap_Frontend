26d08e87aaa9b2023c0eee205ae3ea05108d44c9
Samuel Muli <samuelmuli95@gmail.com>
2024-08-28 19:04:44 +0300

    Add provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/index'
+import store from './redux/index.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
