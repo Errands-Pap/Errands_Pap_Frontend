02502fd6ac5a31400caf4ac5609159a5bc39174e
Samuel Muli <samuelmuli95@gmail.com>
2024-09-25 20:56:50 +0300

    Modify building field

--- src/common/HomeAndBuilding.jsx
+++ src/common/HomeAndBuilding.jsx
@@ -2,10 +2,10 @@ export default function HomeAndBuilding() {
 	return (
 		<div className="w-1/2 flex flex-col">
 			<label className="text-white font-medium self-start mb-2">Building and House Number</label>
-			<div class="flex border border-gray-500 rounded-lg bg-black text-white overflow-hidden">
-				<input type="text" placeholder="Building" className="flex-1 px-4 py-2 bg-[#040409] text-white focus:outline-none"/>
-				<input type="text" placeholder="House Number" className="flex-1 px-4 py-2 bg-[#040409] border-l text-white focus:outline-none"/>
-			</div>
+			<form class="flex border border-gray-500 rounded-lg bg-black text-white overflow-hidden">
+				<input type="text" placeholder="Please tell us your location" className="flex-1 px-4 py-2 bg-[#040409] text-white focus:outline-none"/>
+				<button className="bg-[#b9ff66] px-4 py-2"><span className="material-symbols-outlined text-[#040409] text-2xl">pin_drop</span></button>
+			</form>
 		</div>
 	)
 }
