1e25ecc1a446508cf274bd912482995b07c54fdf
Cedric Muuo <cedric.damian@quinncia.io>
2025-05-16 00:32:23 +0300

    adjust url paths to match backend

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
 		}
 
 		try {
-			const response = await axios.post("http://127.0.0.1:8000/user/register/", {
+			const response = await axios.post("http://127.0.0.1:8000/api/user/register/", {
 				first_name: firstName,
 				last_name: lastName,
 				email: email,
